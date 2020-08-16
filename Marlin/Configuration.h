bb73004b6dd5d6c67c9e440ed408601a2c402e95
grantw99 <grantw99@gmail.com>
2020-08-16 17:49:50 -0400

    bed size fixed for our MPCNC x=33.5in/850mm, y=23.5in/600mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1116,8 +1116,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 850
+#define Y_BED_SIZE 600
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
